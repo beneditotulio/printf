fdfd4c3040cd8ad5f03eab5f78ead8219535e420
AmâncioChiluvane <109949333+amanciochiluvane@users.noreply.github.com>
2022-11-23 06:18:00 +0200

    added

--- main.h
+++ main.h
@@ -3,7 +3,24 @@
 
 #include <stdlib.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include <limits.h>
+#include <unistd.h>
+
+/**
+ * struct call - struct call
+ * @t: t - flag for data type
+ * @f: function - assocated function
+ *
+ */
+typedef struct call
+{
+	char t;
+	int (*f)(char *, va_list, int);
+} call_t;
 
 int _printf(const char *format, ...);
+int buff_append(char *buff_dest, va_list arg, int buff_count, char type);
+int print_buff(char *buff, unsigned int nbuff);
 
 #endif
